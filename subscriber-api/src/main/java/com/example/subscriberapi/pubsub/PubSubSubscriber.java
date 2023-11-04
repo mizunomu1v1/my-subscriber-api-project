1caea20a1bffbcd934e691a33bad24769a0dd0ac
mizunomu1v1 <fulltime.furotime@gmail.com>
2023-11-04 20:20:28 +0900

    処理実装完了、ビルド上手くいかず

--- subscriber-api/src/main/java/com/example/subscriberapi/pubsub/PubSubSubscriber.java
+++ subscriber-api/src/main/java/com/example/subscriberapi/pubsub/PubSubSubscriber.java
@@ -1,27 +1,17 @@
 package com.example.subscriberapi.pubsub;
 
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.support.PubSubHeaders;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.messaging.Message;
-import org.springframework.stereotype.Service;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
+@EnableBinding(Sink.class)
 public class PubSubSubscriber {
-    private final PubSubTemplate pubSubTemplate;
 
-    // Messageを受け取る
-    public PubSubSubscriber(PubSubTemplate pubSubTemplate) {
-        this.pubSubTemplate = pubSubTemplate;
-    }
-
-    // Messageを処理する
-    @ServiceActivator(inputChannel = "pubsubInputChannel")
-    public void receiveMessage() {
-        String payload = pubSubTemplate.getPayload();
-        String messageId = pubSubTemplate.getHeaders().get(PubSubHeaders.MESSAGE_ID, String.class);
-
-        System.out.println("Received message: " + payload);
-        System.out.println("Message ID: " + messageId);
+    @StreamListener(Sink.INPUT)
+    public void receiveMessage(String message) {
+        // メッセージを受信したときの処理
+        System.out.println("Received message: " + message);
     }
 }
